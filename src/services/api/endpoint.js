71a203344977f96cf2c105bb03ef0b0f1612285d
mohamad-ridwan <mr643062@gmail.com>
2022-12-03 16:31:42 +0700

    ridwan3 move to cyclic backend hosting

--- src/services/api/endpoint.js
+++ src/services/api/endpoint.js
@@ -1,4 +1,7 @@
-const endpoint = 'https://hospice-medical.herokuapp.com'
+// const endpoint = 'https://hospice-medical.herokuapp.com'
 // const endpoint = 'http://localhost:6500'
+
+// from cyclic.sh
+const endpoint = 'https://tame-teal-vulture-tie.cyclic.app'
 
 export default endpoint
