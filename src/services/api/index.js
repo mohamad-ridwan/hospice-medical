3ef2881edf1b525825185d536155bc08399741ae
= <=>
2023-12-20 08:50:15 +0700

    commit

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -2,63 +2,63 @@ import fetchJwtToken from "./fetchJwtToken"
 import useFetch from "./useFetch"
 
 // navbar
-const APIGetNavbar = () => useFetch('v1/navbar/get', 'GET')
+const APIGetNavbar = () => useFetch('navbar/get', 'GET')
 
 // headerpage
-const APIGetHeaderPage = () => useFetch('v3/header-page/get', 'GET')
+const APIGetHeaderPage = () => useFetch('header-page/get', 'GET')
 
 // procedure category
-const APIGetProcedureCategory = () => useFetch('v4/procedure-category/get', 'GET')
+const APIGetProcedureCategory = () => useFetch('procedure-category/get', 'GET')
 
 // about myself
-const APIGetAboutMyself = () => useFetch('v5/about-myself/get', 'GET')
+const APIGetAboutMyself = () => useFetch('about-myself/get', 'GET')
 
 // feedback
-const APIGetFeedback = () => useFetch('v6/feedback/get', 'GET')
+const APIGetFeedback = () => useFetch('feedback/get', 'GET')
 
 // our offered services
-const APIGetOurOfferedServices = () => useFetch('v7/our-offered-services/get', 'GET')
+const APIGetOurOfferedServices = () => useFetch('our-offered-services/get', 'GET')
 
 // servicing hours
-const APIGetServicingHours = () => useFetch('v8/servicing-hours/get', 'GET')
-const APIPostFormAppointment = (_id, data) => useFetch(`v8/servicing-hours/post/book-an-appointment/user-appointment-data/${_id}`, 'POST', data)
-const APIPostPatientRegistration = (roleId, data)=>useFetch(`v8/servicing-hours/post/role/${roleId}/data`, 'POST', data)
+const APIGetServicingHours = () => useFetch('servicing-hours/get', 'GET')
+const APIPostFormAppointment = (_id, data) => useFetch(`servicing-hours/post/book-an-appointment/user-appointment-data/${_id}`, 'POST', data)
+const APIPostPatientRegistration = (roleId, data)=>useFetch(`servicing-hours/post/role/${roleId}/data`, 'POST', data)
 
 // blogs
-const APIGetBlogs = () => useFetch('v9/blog/get', 'GET')
-const APIPostComment = (_id, id, data) => useFetch(`v9/blog/post/all-document/data/comments/${_id}/${id}`, 'POST', data)
-const APIDeleteComment = (_idblog, iduser, index) => useFetch(`v9/blog/delete/blog/data/comments/${_idblog}/${iduser}/${index}`, 'DELETE')
-const APIPutProfileUserComment = (id, data)=>useFetch(`v9/blog/put/profile-user-comment/every-article/data/comments/${id}`, 'PUT', data)
+const APIGetBlogs = () => useFetch('blog/get', 'GET')
+const APIPostComment = (_id, id, data) => useFetch(`blog/post/all-document/data/comments/${_id}/${id}`, 'POST', data)
+const APIDeleteComment = (_idblog, iduser, index) => useFetch(`blog/delete/blog/data/comments/${_idblog}/${iduser}/${index}`, 'DELETE')
+const APIPutProfileUserComment = (id, data)=>useFetch(`blog/put/profile-user-comment/every-article/data/comments/${id}`, 'PUT', data)
 
 // footer
-const APIGetFooter = () => useFetch('v2/footer/get', 'GET')
-const APIPostNewsletter = (_id, data) => useFetch(`v2/footer/post/newsletter/users/${_id}`, 'POST', data)
+const APIGetFooter = () => useFetch('footer/get', 'GET')
+const APIPostNewsletter = (_id, data) => useFetch(`footer/post/newsletter/users/${_id}`, 'POST', data)
 
 // doctors
-const APIGetDoctors = () => useFetch('v10/doctors/get', 'GET')
+const APIGetDoctors = () => useFetch('doctors/get', 'GET')
 
 // contact
-const APIGetContact = () => useFetch('v11/contact/get/get-all', 'GET')
-const APIPostContactForm = (_id, data) => useFetch(`v11/contact/post/form-contact-us/data/${_id}`, 'POST', data)
+const APIGetContact = () => useFetch('contact/get/get-all', 'GET')
+const APIPostContactForm = (_id, data) => useFetch(`contact/post/form-contact-us/data/${_id}`, 'POST', data)
 
 //users
-const APIGetUsers = () => useFetch('v12/users/get', 'GET')
-const APIPostUsers = (data) => useFetch(`v12/users/post`, 'POST', data)
-const APIPutIsVerification = (userId, data)=>useFetch(`v12/users/put/${userId}`, 'PUT', data)
-const APIPutUser = (id, data)=>useFetch(`v12/users/put/user/${id}`, 'PUT', data)
+const APIGetUsers = () => useFetch('users/get', 'GET')
+const APIPostUsers = (data) => useFetch(`users/post`, 'POST', data)
+const APIPutIsVerification = (userId, data)=>useFetch(`users/put/${userId}`, 'PUT', data)
+const APIPutUser = (id, data)=>useFetch(`users/put/user/${id}`, 'PUT', data)
 
 // verification
-const APIPostVerification = (data)=>useFetch('v13/verification/post', 'POST', data)
-const APIGetVerification = ()=>useFetch('v13/verification/get', 'GET')
-const APIPutVerification = (userId, data)=>useFetch(`v13/verification/put/${userId}`, 'PUT', data)
-const APIDeleteVerification = (id)=>useFetch(`v13/verification/delete/${id}`, 'DELETE')
+const APIPostVerification = (data)=>useFetch('verification/post', 'POST', data)
+const APIGetVerification = ()=>useFetch('verification/get', 'GET')
+const APIPutVerification = (userId, data)=>useFetch(`verification/put/${userId}`, 'PUT', data)
+const APIDeleteVerification = (id)=>useFetch(`verification/delete/${id}`, 'DELETE')
 // verification create new password and create jwt-token
-const APIPostCreateJwtToken = (userId)=>useFetch(`v13/verification/post/forgot-password/create-new-password/${userId}/user`, 'POST')
-const APIGetJwtTokenVerif = (token)=>fetchJwtToken('v13/verification/get/forgot-password/create-new-password', 'GET', token)
+const APIPostCreateJwtToken = (userId)=>useFetch(`verification/post/forgot-password/create-new-password/${userId}/user`, 'POST')
+const APIGetJwtTokenVerif = (token)=>fetchJwtToken('verification/get/forgot-password/create-new-password', 'GET', token)
 
 // black list JWT
-const APIPostBlackListJWT = (data)=>useFetch(`v15/black-list-jwt/post`, 'POST', data)
-const APIGetBlackListJWT = ()=>useFetch(`v15/black-list-jwt/get`, 'GET')
+const APIPostBlackListJWT = (data)=>useFetch(`black-list-jwt/post`, 'POST', data)
+const APIGetBlackListJWT = ()=>useFetch(`black-list-jwt/get`, 'GET')
 
 const API = {
     APIGetNavbar,
